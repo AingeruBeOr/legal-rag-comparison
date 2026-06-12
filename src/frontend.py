f94f516f03fa27814bf375423da8017bc6aa12f9
Aingeru <1405bellido@gmail.com>
2026-06-12 17:36:52 +0200

    forntend update

--- src/frontend.py
+++ src/frontend.py
@@ -7,23 +7,28 @@ from src.online_pipeline import OnlinePipeline
 from dotenv import load_dotenv
 load_dotenv(".env")
 
-pipeline = OnlinePipeline()
+pipeline = OnlinePipeline(
+    embedding_model_name="BAAI/bge-m3",
+    top_k=3, 
+    collection_name="documents",
+    llm_model_name="gemini-3.1-flash-lite"
+)
 
 def respond(message, chat_history):
     text_response, retrieved_context = pipeline.do_rag(message)
 
-    response_to_render = f"Answer: {text_response}\n\nRetrieved Contexts:\n"
+    response_to_render = f"# Respuesta\n{text_response}\n\n# Contexto recuperado\n"
     for idx, context in enumerate(retrieved_context):
-        response_to_render += f"{idx+1}. (Score: {context.score:.2f}) {context.payload['source_file']}\n\n"
+        response_to_render += f"{idx+1}. (Score: {context.score:.2f}) {context.payload['source_file']} (page {context.payload['page']})\n\n"
 
     chat_history.append({"role": "user", "content": message})
-    chat_history.append({"role": "assistant", "content": text_response})
+    chat_history.append({"role": "assistant", "content": response_to_render})
     return "", chat_history
 
-with gr.Blocks() as demo:
-    chatbot = gr.Chatbot()
-    msg = gr.Textbox()
-    clear = gr.ClearButton([msg, chatbot])
+with gr.Blocks(fill_height=True) as demo:
+    chatbot = gr.Chatbot(scale=2)
+    msg = gr.Textbox(submit_btn=True, scale=1, show_label=False, placeholder="Escribe tu pregunta aquí...")
+    clear = gr.ClearButton([msg, chatbot], value="Limpiar chat")
 
     msg.submit(respond, [msg, chatbot], [msg, chatbot])
 
